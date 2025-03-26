70670db0ff3c2d0d8ab1e84df3ce0022de5ca4cf
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-03-26 06:10:33 +0200

    new dataset added xx

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
 // const flaskApiUrl = 'http://127.0.0.1:5000/api';
 // const laravelApiUrl = 'http://127.0.0.1:8000/api';
 
-const flaskApiUrl ='https0://pmsem-flask-api-production.up.railway.app/api';
+const flaskApiUrl ='https://pmsem-flask-api-production.up.railway.app/api';
 const laravelApiUrl ="https://pmsemlaravelapi.up.railway.app/api";
 
 export { flaskApiUrl, laravelApiUrl };
