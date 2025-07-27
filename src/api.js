53f2ab1aa787098892791b036e758829198b274b
Mariam0401 <umutesimariam04@gmail.com>
2025-07-27 08:10:28 +0200

    some codes added

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 // import axios from 'axios';
 
-const flaskApiUrl = 'http://127.0.0.1:5000/api';
-const laravelApiUrl = 'http://127.0.0.1:8000/api';
+// const flaskApiUrl = 'http://127.0.0.1:5000/api';
+// const laravelApiUrl = 'http://127.0.0.1:8000/api';
 
-// const flaskApiUrl ='https://pmsem-flask-api-production.up.railway.app/api';
-// const laravelApiUrl ="https://pmsemlaravelapi-production.up.railway.app/api";
+const flaskApiUrl ='https://pmsem-flask-api-production.up.railway.app/api';
+const laravelApiUrl ="https://pmsemlaravelapi-production.up.railway.app/api";
 
 export { flaskApiUrl, laravelApiUrl };
